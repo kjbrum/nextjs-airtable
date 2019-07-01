ddd1ecf6c2cc5e156dda97028e59c152cc61b975
Kyle Brumm <kjbrumm@gmail.com>
2019-06-30 23:23:19 -0500

    Add now config files

--- next.config.js
+++ next.config.js
@@ -2,17 +2,16 @@ const { parsed: localEnv } = require('dotenv').config()
 const webpack = require('webpack')
 
 module.exports = {
-	// target: 'serverless',
-	webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-		config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-
-		return config
-	},
-	exportPathMap: () => {
-		return {
-			'/': {
-				page: '/',
-			},
-		}
-	},
+	target: 'serverless',
+	// webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+	// 	config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+	// 	return config
+	// },
+	// exportPathMap: () => {
+	// 	return {
+	// 		'/': {
+	// 			page: '/',
+	// 		},
+	// 	}
+	// },
 }
--- server.js
+++ server.js
@@ -148,3 +148,5 @@ const getAirtablePost = recordId => {
 		)
 	})
 }
+
+module.exports = app
